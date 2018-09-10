bf1e3157feaab5b1121ef21d16e64e1afa7710fc
shangguanyunluo <zhang.yafeng@foxmail.com>
2018-09-10 13:29:47 +0800

    add a apply function

---
+++ 001CoreLib/builtin_lib/builtin_apply_example_1.py
@@ -0,0 +1,15 @@
+# encoding:utf-8
+'''
+Created on 2018年9月10日
+
+@author: litsoft
+'''
+
+
+def function(a, b):
+    print a, b
+    
+
+apply(function, ("hello", "world!"))
+    
+apply(function, (1, 2 + 3))
